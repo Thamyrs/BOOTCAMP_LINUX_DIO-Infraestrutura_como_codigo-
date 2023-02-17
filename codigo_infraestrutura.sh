963567e0db17486a8a2bba4b1aea0520299f5f54
Thamyrs <108243566+Thamyrs@users.noreply.github.com>
2023-02-17 10:49:29 -0300

    Alterando o algoritmo de hashing das senhas
    O comando openssl passwd estava sendo usado sem parâmetros, e ele estava atribuindo por default o algoritmo de hashing MD5. 
    Atribuí o parâmetro -5 para ele usar o SHA-256.

--- codigo_infraestrutura.sh
+++ codigo_infraestrutura.sh
@@ -9,33 +9,33 @@ groupadd GRP_VEN
 groupadd GRP_SEC
 
 echo "Criando usuários do grupo adm..."
-useradd carlos -m -c "Carlos" -s /bin/bash -p $(openssl passwd senha123) -G GRP_ADM
+useradd carlos -m -c "Carlos" -s /bin/bash -p $(openssl passwd -5 senha123) -G GRP_ADM
 passwd carlos -e
 
-useradd maria -m -c "Maria" -s /bin/bash -p $(openssl passwd senha123) -G GRP_ADM
+useradd maria -m -c "Maria" -s /bin/bash -p $(openssl passwd -5 senha123) -G GRP_ADM
 passwd maria -e
 
-useradd joao -m -c "João" -s /bin/bash -p $(openssl passwd senha123) -G GRP_ADM
+useradd joao -m -c "João" -s /bin/bash -p $(openssl passwd -5 senha123) -G GRP_ADM
 passwd joao -e
 
 echo "Criando usuários do grupo ven..."
-useradd debora -m -c "Débora" -s /bin/bash -p $(openssl passwd senha123) -G GRP_VEN
+useradd debora -m -c "Débora" -s /bin/bash -p $(openssl passwd -5 senha123) -G GRP_VEN
 passwd debora -e
 
-useradd sebastiana -m -c "Sebastiana" -s /bin/bash -p $(openssl passwd senha123) -G GRP_VEN
+useradd sebastiana -m -c "Sebastiana" -s /bin/bash -p $(openssl passwd -5 senha123) -G GRP_VEN
 passwd sebastiana -e
 
-useradd roberto -m -c "Roberto" -s /bin/bash -p $(openssl passwd senha123) -G GRP_VEN
+useradd roberto -m -c "Roberto" -s /bin/bash -p $(openssl passwd -5 senha123) -G GRP_VEN
 passwd roberto -e
 
 echo "Criando usuários do grupo sec..."
-useradd josefina -m -c "Josefina" -s /bin/bash -p $(openssl passwd senha123) -G GRP_SEC
+useradd josefina -m -c "Josefina" -s /bin/bash -p $(openssl passwd -5 senha123) -G GRP_SEC
 passwd josefina -e
 
-useradd amanda -m -c "Amanda" -s /bin/bash -p $(openssl passwd senha123) -G GRP_SEC
+useradd amanda -m -c "Amanda" -s /bin/bash -p $(openssl passwd -5 senha123) -G GRP_SEC
 passwd amanda -e
 
-useradd rogerio -m -c "Rogério" -s /bin/bash -p $(openssl passwd senha123) -G GRP_SEC
+useradd rogerio -m -c "Rogério" -s /bin/bash -p $(openssl passwd -5 senha123) -G GRP_SEC
 passwd rogerio -e
 
 echo "Mundando permissões dos diretórios..."
